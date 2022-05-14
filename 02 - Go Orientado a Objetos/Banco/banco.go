bd01500bb2cf10103addf88b863aa447cb081346
johnny.junior <johnny.junior@pentacom.solutions>
2022-05-14 20:51:59 -0300

    learn how to use pointers and functions with many parameters

--- 02 - Go Orientado a Objetos/Banco/banco.go
+++ 02 - Go Orientado a Objetos/Banco/banco.go
@@ -26,4 +26,51 @@ func main() {
 
 	fmt.Println(contaDoJohnny)
 	fmt.Println(contaDaAmanda)
+
+	//criando variaveis nulas
+	var s *string = nil
+	fmt.Println(s)
+
+	/*
+		Ponteiros:
+	*/
+	var contaDaCris *ContaCorrente
+	contaDaCris = new(ContaCorrente)
+	contaDaCris.titular = "Cris"
+
+	contaDoJohnny2 := ContaCorrente{
+		titular:       "Johnny",
+		numeroAgencia: 589,
+		numeroConta:   123456,
+		saldo:         125.50,
+	}
+	fmt.Println(contaDoJohnny == contaDoJohnny2)
+
+	contaDoJohnny2.numeroConta = 899
+	fmt.Println(contaDoJohnny == contaDoJohnny2)
+
+	var contaDaCris2 *ContaCorrente
+	contaDaCris2 = new(ContaCorrente)
+	contaDaCris2.titular = "Cris"
+	fmt.Println(contaDaCris)
+	fmt.Println(contaDaCris2)
+	fmt.Println(&contaDaCris)
+	fmt.Println(&contaDaCris2)
+	fmt.Println(contaDaCris == contaDaCris2)
+	fmt.Println(*contaDaCris == *contaDaCris2)
+
+	//
+	fmt.Println(Somando(1))
+	fmt.Println(Somando(1, 1))
+	fmt.Println(Somando(1, 1, 1))
+	fmt.Println(Somando(1, 1, 2, 4))
+}
+
+//função com parametros inderterminados
+func Somando(numeros ...int) int {
+	resultadoDaSoma := 0
+	for _, numero := range numeros {
+		resultadoDaSoma += numero
+	}
+	return resultadoDaSoma
 }
